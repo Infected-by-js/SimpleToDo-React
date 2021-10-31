c7a593757b724eb65601ded2d7860eb5aaec8d4e
infected-by-js <infectedbyjs@gmail.com>
2021-10-31 22:23:26 +1000

    added signup component

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,14 +1,24 @@
 // sidebar
-export { Arrow } from './iconArrow.jsx';
-export { Home } from './iconHome.jsx';
-export { Tasks } from './iconTasks.jsx';
-export { Groups } from './iconGroups.jsx';
-export { Stats } from './iconStats.jsx';
-export { Calendar } from './iconCalendar.jsx';
-export { Settings } from './iconSettings.jsx';
-export { LogOut } from './iconLogOut.jsx';
-export { Plus } from './iconPlus.jsx';
+export { Arrow } from './iconArrow';
+export { Overview } from './iconOverview';
+export { Groups } from './iconGroups';
+export { Stats } from './iconStats';
+export { Calendar } from './iconCalendar';
+export { Settings } from './iconSettings';
+export { LogOut } from './iconLogOut';
+export { Plus } from './iconPlus';
 
 //themes
 export { ThemeDark } from './iconThemeDark';
 export { ThemeLight } from './iconThemeLight';
+
+//social
+export { Google } from './IconGoogle';
+export { Github } from './IconGithub';
+export { Anonymous } from './IconAnonymous';
+
+// form
+export { FormUser } from './IconFormUser';
+export { FormPassword } from './IconFormPassword';
+export { FormMail } from './IconFormMail';
+export { FormEye } from './IconFormEye';
